1fa834850f6a7f267b2f2f2719809f4c1b37ced4
ferlimav <f172375@dac.unicamp.br>
2020-10-30 23:48:41 -0300

    nova atv unity

---
+++ New Unity Project/Assets/scripts/controle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controle : MonoBehaviour
+{
+    public Vector3 direção;
+    public float força;
+    Rigidbody Rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Rb.AddForce(direção * força);
+        }
+    }
+}
---
+++ New Unity Project/Assets/scripts/controle2.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controle2 : MonoBehaviour
+{
+    public Vector3 direção;
+    public float velocidade;
+    Rigidbody Rb;
+
+    private void Awake()
+    {
+        Rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        direção = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+        transform.Translate(direção * velocidade * Time.deltaTime);
+    }
+}
